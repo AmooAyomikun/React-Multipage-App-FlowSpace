e2acbbd8c15610d3e540ceae43fb18952c842288
Amoo Quadri <amooquadri555@gmail.com>
2026-06-17 13:58:50 +0100

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/React-Multipage-App-FlowSpace'>
       <App />
     </BrowserRouter>
   </StrictMode>,
